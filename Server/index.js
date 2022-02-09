ad927fb99ad67dc1784246bed0dba4bb8823510b
Judson S <judz1105@gmail.com>
2022-02-09 08:49:36 -0800

    hi

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
+const path = require('path')
 const app = express();
+const PORT = 4000;
+
 app.use(cors());
-app.listen(4000, () => {
+app.listen(PORT, () => {
     console.log('Server Works !!! At port 4000');
 });
+
+app.get('/', function(req,res) {
+    res.sendFile(path.join(__dirname,"../index.html"))
+    app.use(express.static(path.join(__dirname, "../public")))
+});
+
 app.get('/download', (req,res) => {
     var URL = req.query.URL;
 
