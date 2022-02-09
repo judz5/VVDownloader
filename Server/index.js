02568ce1920042896d502e8f8f6b0cd1e50564fe
Judson Salinas <judz1105@gmail.com>
2022-02-08 21:57:51 -0600

    gradient

--- Server/index.js
+++ Server/index.js
@@ -9,7 +9,7 @@ app.listen(4000, () => {
 app.get('/download', (req,res) => {
     var URL = req.query.URL;
 
-    res.header('Content-Disposition', 'attachment; filename=YTDL.title');
+    res.header('Content-Disposition', 'attachment; filename=video.mp4');
 
     ytdl(URL, {
     format: 'mp4'
